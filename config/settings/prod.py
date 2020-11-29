aa9d241b781bbe227d730be620e5ee823dc05f1c
Layo <lionelmena@gmail.com>
2020-11-29 20:46:33 +0100

    [Added] Django settings for production

---
+++ config/settings/prod.py
@@ -0,0 +1,11 @@
+"""
+Django settings for the production environment.
+"""
+from .base import *  # noqa
+
+
+# GENERAL
+# ------------------------------------------------------------------------------
+DEBUG = False
+
+ALLOWED_HOSTS = ["www.urbandata.com"]
